4110fa7cb08a62672c6195ab6a07deab848dc0f6
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-04-13 17:30:40 +0300

    Finished annotation_app

--- annotation_app.py
+++ annotation_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import random
 import json
+import pandas as pd
+from collections import defaultdict
 
 st.header('Interactive image captioning annotation')
 st.subheader('Instructions')
@@ -21,28 +23,90 @@ st.markdown('**Reformulation:** A pizza sitting on a plate on a wooden table')
 
 st.subheader('Annotation')
 
-with open('data_example.json', 'r') as fp:
-    data = json.load(fp)
+# Option 1: with a local file
+# data_file_name = 'data_example.json'
+# with open(data_file_name, 'r') as fp:
+#     data = json.load(fp)
 
-res = {}
-reformulation = ''
-while reformulation is not None and len(res) < len(data):
-    reformulation = None
-    sample = random.sample(data, 1)[0]
+# reformulation = 'a'
+# not_visited_inds = [i for i in range(len(data)) if len(data[i]['reformulations']) == 0]
+# st.markdown('DEBUG ' + str(len(reformulation)))
+# st.markdown('DEBUG ' + str(len(not_visited_inds)))
+# while len(reformulation) > 0 and len(not_visited_inds) > 0:
+#     reformulation = ''
+#     sample_ind = random.sample(not_visited_inds, 1)[0]
+#     sample = data[sample_ind]
+#     url_prefix = 'http://images.cocodataset.org/'
+#     url_prefix += sample['split'] + '2017/'
+#     image_url = url_prefix + str(sample['image_id']).zfill(12) + '.jpg'
+#     st.image(image_url, width=350)
+#     st.markdown('**Model generated caption:** ' + sample['caption'])
+#     reformulation = st.text_input(label='**Reformulation:**')
+
+#     if len(reformulation) > 0:
+#         st.markdown('DEBUG HERE')
+#         data[sample_ind]['reformulations'].append(reformulation)
+#         json_obj = json.dumps(data)
+#         with open(data_file_name, 'w') as fp:
+#             fp.write(json_obj)
+
+# Option 2: with google sheets
+# def load_data(sheets_url):
+#     csv_url = sheets_url.replace("edit?usp=sharing", "gviz/tq?tqx=out:csv&sheet=Sheet1")
+#     return pd.read_csv(csv_url)
+
+# def create_image_to_count():
+#     df = load_data(st.secrets["public_gsheets_url"])
+#     image_id_list = df['image_id'].tolist()
+#     res = defaultdict(int)
+#     for image_id in image_id_list:
+#         res[image_id] += 1
+#     return res
+
+# st.markdown('DEBUG ' + str(create_image_to_count()))
+
+# df = pd.concat((df, pd.DataFrame({'split': ['train'], 'image_id': ['6666'], 'reformulation': ['A dog']})))
+
+# Option 3: with streamlit state
+state = st.session_state
+
+if "annotations" not in state:
+    data_file_name = 'data_example.json'
+    with open(data_file_name, 'r') as fp:
+        data = json.load(fp)
+
+    state.annotations = {}
+    state.samples = data
+    state.current_sample = state.samples[0]
+
+def annotate():
+    reformulation = state.ref_text_box
+    st.markdown('DEBUG in annotate with ' + reformulation)
+    image_id = state.current_sample['image_id']
+    if image_id not in state.annotations:
+        state.annotations[image_id] = []
+    state.annotations[image_id].append(reformulation)
+    state.samples.remove(state.current_sample)
+    if len(state.samples) > 0:
+        state.current_sample = state.samples[0]
+
+if len(state.samples) > 0:
+    sample = state.current_sample
+
     url_prefix = 'http://images.cocodataset.org/'
     url_prefix += sample['split'] + '2017/'
     image_url = url_prefix + str(sample['image_id']).zfill(12) + '.jpg'
     st.image(image_url, width=350)
     st.markdown('**Model generated caption:** ' + sample['caption'])
-    reformulation = st.text_input(label='**Reformulation:**')
+    st.text_input(label='**Reformulation:**', on_change=annotate, key='ref_text_box')
 
-    if reformulation != None:
-        new_image_id = sample['image_id']
-        if sample['split'] == 'train':
-            new_image_id = 1000000 + new_image_id
-        else:
-            new_image_id = 2000000 + new_image_id
-        if new_image_id not in res:
-            res[new_image_id] = []
-        res[new_image_id].append(reformulation)
-        reformulation = None
+else:
+    st.info("Everything annotated.")
+
+st.info(f"Annotated: {len(state.annotations)}, Remaining: {len(state.samples)}")
+
+st.download_button(
+    "Download annotations as CSV",
+    "\n".join([f"{k}\t{v}" for k, v in state.annotations.items()]),
+    file_name="export.csv",
+)
